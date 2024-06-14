af4692ae1c3eb8db6edd51fc535a4e56b4bc5f76
Costbound <popovskiy96@gmail.com>
2024-06-14 06:45:02 +0000

    add find all contacts functionality

--- src/constants/localDB.js
+++
@@ -1,9 +0,0 @@
-import path from 'node:path';
-
-export const PATH_DB = path.join(
-  process.cwd(),
-  'src',
-  'db',
-  'localDB',
-  'contacts.json',
-);
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,23 +1,22 @@
-import { PATH_DB } from '../constants/localDB.js';
-import fs from 'node:fs/promises';
+import { ContactsCollection } from '../db/models/contacts.js';
 
 export const getAllContacts = async () => {
-  const dbJson = await fs.readFile(PATH_DB, 'utf8');
-  return JSON.parse(dbJson);
+  const contacts = await ContactsCollection.find();
+  return JSON.parse(contacts);
 };
 
-export const getContactById = async (id) => {
-  const dbJson = await fs.readFile(PATH_DB, 'utf8');
-  const contacts = JSON.parse(dbJson);
-  const contact = contacts.find((contact) => contact.id === id);
-  if (contact) {
-    return {
-      status: 200,
-      data: contact,
-    };
-  } else {
-    return {
-      status: 404,
-    };
-  }
-};
+// export const getContactById = async (id) => {
+//   const dbJson = await fs.readFile(PATH_DB, 'utf8');
+//   const contacts = JSON.parse(dbJson);
+//   const contact = contacts.find((contact) => contact.id === id);
+//   if (contact) {
+//     return {
+//       status: 200,
+//       data: contact,
+//     };
+//   } else {
+//     return {
+//       status: 404,
+//     };
+//   }
+// };
