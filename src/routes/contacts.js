9628b7b578859823a166c99facfc6dd47fe920bf
Costbound <popovskiy96@gmail.com>
2024-06-15 00:32:27 +0000

    fix contact by ID router

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { getContactsController } from '../controllers/contacts.js';
+import {
+  getContactsByIdController,
+  getContactsController,
+} from '../controllers/contacts.js';
 
 const router = Router();
 
 router.get('/contacts', getContactsController);
 
-router.get('/contacts/:contactId', getContactsController);
+router.get('/contacts/:contactId', getContactsByIdController);
 
 export default router;
