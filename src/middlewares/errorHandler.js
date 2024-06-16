666adacbf8e371f8977bdb8de5bf2a032baa3494
Costbound <popovskiy96@gmail.com>
2024-06-16 06:19:20 +0000

    troubleshooting errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,21 +2,21 @@ import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res) => {
   console.log(`ErrorHandler: err is`, err);
-  if (err instanceof HttpError) {
-    // const responseData = {
-    //   status: err.status,
-    //   message: err.name,
-    //   data: { message: err.message },
-    // };
-    // if (err.errors) {
-    //   responseData.data.errors = err.errors;
-    // }
-    res.status(err.status).json({
-      status: err.status,
-      message: err.name,
-      data: { message: err.message },
-    });
-  }
+  // if (err instanceof HttpError) {
+  //   // const responseData = {
+  //   //   status: err.status,
+  //   //   message: err.name,
+  //   //   data: { message: err.message },
+  //   // };
+  //   // if (err.errors) {
+  //   //   responseData.data.errors = err.errors;
+  //   // }
+  //   res.status(err.status).json({
+  //     status: err.status,
+  //     message: err.name,
+  //     data: { message: err.message },
+  //   });
+  // }
   res.status(500).json({
     satus: 500,
     message: 'Something went wrong',
