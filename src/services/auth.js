d3b77a0496ef96be6e89fb602a887e4e437fffc0
Costbound <popovskiy96@gmail.com>
2024-06-19 06:59:12 +0000

    add login functionality

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,9 @@
 import createHttpError from 'http-errors';
 import { UsersCollection } from '../db/models/user.js';
 import bcrypt from 'bcrypt';
+import { randomBytes } from 'crypto';
+import { SessionsCollection } from '../db/models/session.js';
+import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../constants/index.js';
 
 export const registerUser = async (payload) => {
   const user = await UsersCollection.findOne({ email: payload.email });
@@ -11,5 +14,28 @@ export const registerUser = async (payload) => {
   return await UsersCollection.create({
     ...payload,
     password: hashedPassword,
+  });
+};
+
+export const loginUser = async (payload) => {
+  const user = await UsersCollection.findOne({ email: payload.email });
+
+  if (!user) throw createHttpError(401);
+
+  const isPasswordEqual = bcrypt.compare(payload.password, user.password);
+
+  if (!isPasswordEqual) throw createHttpError(401);
+
+  await SessionsCollection.deleteOne({ userId: user._id });
+
+  const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+  return await SessionsCollection.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + THIRTY_DAYS),
   });
 };
